24496293c0403b7c64e464761da499514400958b
andresahumadaa <2268@holbertonschool.com>
2021-05-13 23:18:36 +0000

    f nop

---
+++ more_functions.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+/**
+ * swap - Swaps the top two elements of the stack.
+ * @head: Head of the list
+ * @counter: Number of the line
+ * Return: Void - Nothing
+ **/
+void swap(stack_t **head, unsigned int counter)
+{
+int tmp;
+if (head == NULL || *head == NULL || (*head)->next == NULL)
+{
+fprintf(stderr, "L%u: can't swap, stack too short\n", counter);
+exit(EXIT_FAILURE);
+}
+tmp = (*head)->n;
+(*head)->n = (*head)->next->n;
+(*head)->next->n = tmp;
+}
+/**
+ * nop - Doesn’t do anything.
+ * @head: Head of the list
+ * @counter: Number of the line
+ * Return: Void - Nothing
+ **/
+void nop(stack_t **head, unsigned int counter)
+{
+(void) head;
+(void) counter;
+}
